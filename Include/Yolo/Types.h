597d52bfdd0e771056fb8863b8fda1d713ef5184
MaiHD <maihd.dev@gmail.com>
2019-12-08 15:17:37 +0700

    Lambda binding and Function<>

--- Include/Yolo/Types.h
+++ Include/Yolo/Types.h
@@ -13,6 +13,51 @@ using uint64  = unsigned long long;
 
 using string  = const char*;
 using Handle  = unsigned int;
+using NullPtr = decltype(nullptr);
+
+template <typename T>
+struct Function;
+
+template <typename R, typename ...Args>
+struct Function<R(Args...)>
+{
+    using Executor = R(*)(void*, Args...);
+
+    void*       context;
+    Executor    executor;
+
+    static R ExecuteFunction(void* context, Args... args)
+    {
+        using Function = R(*)(Args...);
+        return ((Function)context)(args...);
+    };
+
+    template <typename T>
+    static R ExecuteLambda(void* context, Args... args)
+    {
+        return (*(T*)&context)(args...);
+    };
+
+    inline Function(R(*function)(Args...))
+    {
+        this->context = function;
+        this->executor = &ExecuteFunction;
+    }
+
+    template <typename T>
+    inline Function(T lambda)
+    {
+        static_assert(sizeof(lambda) <= sizeof(void*), "Lambda is too big, it must be <= sizeof(void*). We only should one variable binding, because closure is undefined behaviour.");
+
+        this->context = *(void**)&lambda;
+        this->executor = &ExecuteLambda<T>;
+    }
+
+    inline R operator()(Args... args)
+    {
+        return this->executor(this->context, args...);
+    }
+};
 
 namespace Const
 {
@@ -38,4 +83,40 @@ namespace Const
 
     constexpr Handle    EMPTY_HANDLE = 0;
     constexpr string    EMPTY_STRING = "";
+}
+
+template <typename R, typename ...Args>
+inline bool operator==(Function<R(Args...)> a, Function<R(Args...)> b)
+{
+    return a.context == b.context;
+}
+
+template <typename R, typename ...Args>
+inline bool operator!=(Function<R(Args...)> a, Function<R(Args...)> b)
+{
+    return a.context != b.context;
+}
+
+template <typename R, typename ...Args>
+inline bool operator==(Function<R(Args...)> a, NullPtr)
+{
+    return a.executor == nullptr;
+}
+
+template <typename R, typename ...Args>
+inline bool operator==(NullPtr, Function<R(Args...)> b)
+{
+    return b.executor == nullptr;
+}
+
+template <typename R, typename ...Args>
+inline bool operator!=(Function<R(Args...)> a, NullPtr)
+{
+    return a.executor != nullptr;
+}
+
+template <typename R, typename ...Args>
+inline bool operator!=(NullPtr, Function<R(Args...)> b)
+{
+    return b.executor != nullptr;
 }
--- Tests/Main.cc
+++ Tests/Main.cc
@@ -7,6 +7,7 @@
 #include <Yolo/Graphics.h>
 
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
